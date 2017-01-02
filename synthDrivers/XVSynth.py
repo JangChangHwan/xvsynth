a76f2fa4cf1cef26894c46027b7361e1d049578b
JangChangHwan <462356@gmail.com>
2017-01-02 22:01:07 +0900

    v0.2.0 released

--- synthDrivers/XVSynth.py
+++ synthDrivers/XVSynth.py
@@ -1,19 +1,39 @@
 ﻿# coding: utf-8
-# ver 0.1.1
+# ver 0.2.0
 
-import xv
+from comtypes.client import CreateObject
+import math
 import speech
 import synthDriverHandler, os, config, re, logging
 from logHandler import log
-from synthDriverHandler import SynthDriver,VoiceInfo,BooleanSynthSetting
+from synthDriverHandler import SynthDriver, VoiceInfo, BooleanSynthSetting
 import languageHandler
 from collections import OrderedDict
 
-minValue = 0
-maxValue = 10
+
+
+ctrl = None
+si = None
+
+def InitializeXVSynth():
+	try:
+		global ctrl, si
+		obj = CreateObject('SenseReader.Application')
+		ctrl = obj.SpeechControlers.Add('test')
+		ctrl.Init('')
+		si = ctrl.NewSpeakInfo()
+		if ctrl is not None and si is not None:
+			return True
+		else:
+			return False
+	except:
+		return False
+
+
 
 class SynthDriver(synthDriverHandler.SynthDriver):
- supportedSettings = [SynthDriver.PitchSetting()]
+ supportedSettings = [SynthDriver.PitchSetting(), SynthDriver.RateSetting(), SynthDriver.VolumeSetting()]
+
 # supportedSettings=(SynthDriver.VoiceSetting(),SynthDriver.VariantSetting(),SynthDriver.RateSetting(), SynthDriver.PitchSetting(), SynthDriver.InflectionSetting(), SynthDriver.VolumeSetting())
  description = 'SenseReader Synth'
  name = 'XVSynth'
@@ -21,48 +41,79 @@ class SynthDriver(synthDriverHandler.SynthDriver):
 
  @classmethod
  def check(cls):
-  return xv.InitializeXVSynth()
+  return InitializeXVSynth()
 
  def __init__(self):
-  try:
-   lang = languageHandler.getLanguage()
-   self.speakingLanguage=lang
-   xv.si.Speed = 2
-   xv.si.Volume = 4
-   xv.si.Pitch = 4
-  except:
-   pass
+  lang = languageHandler.getLanguage()
+  self.speakingLanguage=lang
 
  def speak(self,speechSequence):
   try:
    for item in speechSequence:
     if isinstance(item, basestring):
-     xv.ctrl.Speak(item, xv.si)
+     ctrl.Speak(item, si)
   except:
    pass
 
  def speakText(self, test, index=None):
   try:
    if isinstance(text, basestring):
-    xv.ctrl.Speak(text, xv.si)
+    ctrl.Speak(text, si)
   except:
    pass
 
  def pause(self,switch):
   try:
-   if not xv.ctrl.Speaking: return
-   xv.ctrl.Stop()
+   if not ctrl.Speaking: return
+   ctrl.Stop()
   except:
    pass
  def cancel(self):
   try:
-   if not xv.ctrl.Speaking: return
-   xv.ctrl.Stop()
+   if not ctrl.Speaking: return
+   ctrl.Stop()
   except:
    pass
 
  def _get_rate(self):
-  pass
+  try:
+   return si.Speed * 5
+  except:
+   pass
+
 
  def _set_rate(self, vl):
-  pass
+  try:
+   si.Speed = int(math.ceil(vl * 0.2))
+  except:
+   pass
+ 
+
+
+ def _get_volume(self):
+  try:
+   return si.Volume * 5
+  except:
+   pass
+
+
+ def _set_volume(self, vl):
+  try:
+   si.Volume = int(math.ceil(vl * 0.2))
+  except:
+   pass
+
+
+
+ def _get_pitch(self):
+  try:
+   return si.Pitch * 5
+  except:
+   pass
+
+
+ def _set_pitch(self, vl):
+  try:
+   si.Pitch = int(math.ceil(vl * 0.2))
+  except:
+   pass
