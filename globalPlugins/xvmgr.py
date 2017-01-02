1722c45fffeac21d226f92c3cec7134e27c2ac99
JangChangHwan <462356@gmail.com>
2017-01-03 02:55:00 +0900

    0.3.3 released

--- globalPlugins/xvmgr.py
+++ globalPlugins/xvmgr.py
@@ -1,12 +1,12 @@
 ﻿# coding: utf-8
-import os
+
 import globalPluginHandler
-from synthDriverHandler import *
+from synthDriverHandler import setSynth, getSynth
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def __init__(self):
 		super(GlobalPlugin, self).__init__()
 
 		obj = getSynth()
-		if obj.name != 'XVSynth': return
-		setSynth('espeak')
+		if obj.name == 'XVSynth' and obj.xv.ctrl == None:
+			setSynth('espeak')
